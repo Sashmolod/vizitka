7e324225a30ced555073e60b0b37ba52d003850c
user3 <user3@gmail.com>
2023-10-02 17:14:56 +0300

    add readme

--- server/config.js
+++ server/config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
-const port = process.env.PORT;
-const secretKey = process.env.SECRET_KEY;
-const storedUsername = process.env.STORED_USERNAME;
-const storedPassword = process.env.STORED_PASSWORD;
+const port = process.env.PORT || 3050;
+const secretKey = process.env.SECRET_KEY || 'admin';
+const storedUsername = process.env.STORED_USERNAME || 'admin';
+const storedPassword = process.env.STORED_PASSWORD || 'admin';
 
 module.exports = {
   port,
